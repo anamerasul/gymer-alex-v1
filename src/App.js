430936f3edd309e708948efa3656d27744e47abf
anamerasul <anamerasul@gmail.com>
2022-04-17 01:55:45 +0600

    react tostify install

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
+  const notify = () => toast("Wow so easy !");
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-
+      <button onClick={notify}>Notify !</button>
+      <ToastContainer />
     </div>
   );
 }
