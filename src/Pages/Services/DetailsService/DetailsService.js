ceebcd5828b7de7b64211d19ffbcc60a93959540
anamerasul <anamerasul@gmail.com>
2022-04-18 13:52:50 +0600

    services component update

--- src/Pages/Services/DetailsService/DetailsService.js
+++ src/Pages/Services/DetailsService/DetailsService.js
@@ -37,10 +37,11 @@ const DetailsService = () => {
     return (
         <div className="App">
             <div className="bg-gray-900 px-5 md:px-0 mx-auto">
-                <h2 className=" py-10 text-center text-3xl font-bold text-blue-600">{singleService?.name}</h2>
+                <h2 className=" py-10 text-center text-3xl font-bold text-blue-600"> Name : {singleService?.name}</h2>
                 <img className="mx-auto rounded-box " src={singleService?.image} alt="" />
-                <p className="text-gray-100 pt-5 w-5/6 lg:w-2/5 mx-auto pb-20 text-center">{singleService?.subtitle} </p>
-                <NavLink className="text-center" to="/contact"><button className="px-8 py-3 mx-auto mb-10 text-center bg-blue-600 text-white  rounded hover:bg-white hover:text-gray-800 transition duration-300 ">Request This Service</button></NavLink>
+                <p className="text-gray-100 pt-5 w-5/6 lg:w-2/5 mx-auto pb-20 text-center"> Description : {singleService?.subtitle} </p>
+                <p className="mx-auto pb-20 text-center"> price : {singleService?.price} </p>
+                <NavLink className="text-center" to="/checkout"><button className="px-8 py-3 mx-auto mb-10 text-center bg-blue-600 text-white  rounded hover:bg-white hover:text-gray-800 transition duration-300 ">Request This Service</button></NavLink>
             </div>
 
         </div>
--- src/Pages/Services/Service/Service.js
+++ src/Pages/Services/Service/Service.js
@@ -3,12 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 const Service = (props) => {
 
-    const { name, subtitle, image, id } = props.service
+    const { name, subtitle, image, id, price } = props.service
     return (
         <div className="transform bg-[#191f2e] hover:-translate-y-3 to-hover hover:bg-gray-800 text-center secondary-bg transition duration-300 rounded w-full shadow-lg mx-auto">
             <img className="mx-auto py-10   rounded-t" src={image} alt="" />
             <h1 className="px-5 pt-5 text-2xl font-bold text-blue-500">{name}</h1>
-            <h2 className="px-5 pt-5 text-gray-200">{subtitle}</h2>
+            <h2 className="px-5 pt-5 text-gray-200">{subtitle.slice(0, 30)}{",,,,,"}</h2>
+            <h2 className="px-5 pt-5 text-gray-200"> price: {price}</h2>
             <NavLink
                 to={`/services/${id}`}
             ><button className="px-8 py-3 mt-10 mb-8 bg-blue-600 text-white rounded hover:bg-white hover:text-gray-800 transition duration-300">VIEW DETAILS</button></NavLink>
