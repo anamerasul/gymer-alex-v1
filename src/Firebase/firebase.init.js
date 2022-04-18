b1c370ecacbeca9159c68a66c96896f97669582c
anamerasul <anamerasul@gmail.com>
2022-04-18 19:32:07 +0600

    env file create

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -8,13 +8,22 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBY_iyLTLt0IiaoyMWEtX3R848W0jOxVro",
-    authDomain: "gymer-alex.firebaseapp.com",
-    projectId: "gymer-alex",
-    storageBucket: "gymer-alex.appspot.com",
-    messagingSenderId: "343551885327",
-    appId: "1:343551885327:web:7585859947b042a618637a",
-    measurementId: "G-CKGKW7WY95"
+    // apiKey: "AIzaSyBY_iyLTLt0IiaoyMWEtX3R848W0jOxVro",
+    // authDomain: "gymer-alex.firebaseapp.com",
+    // projectId: "gymer-alex",
+    // storageBucket: "gymer-alex.appspot.com",
+    // messagingSenderId: "343551885327",
+    // appId: "1:343551885327:web:7585859947b042a618637a",
+    // measurementId: "G-CKGKW7WY95"
+
+
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
